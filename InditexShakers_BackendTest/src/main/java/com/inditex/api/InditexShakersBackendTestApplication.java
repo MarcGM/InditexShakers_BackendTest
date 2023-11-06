390a3a390796e83b0b7df1ecccf7310e11a478b4
MarcGM <3913407+MarcGM@users.noreply.github.com>
2023-11-06 22:01:55 +0100

    First commit

---
+++ InditexShakers_BackendTest/src/main/java/com/inditex/api/InditexShakersBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexShakersBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexShakersBackendTestApplication.class, args);
+	}
+
+}
